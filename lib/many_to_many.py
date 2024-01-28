b720c1a07efc86d85c0ab87e006130fbda39d1bf
Sasha <georgefbrockman@gmail.com>
2024-01-27 20:17:59 -0600

    finishes lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,61 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in Contract.all if contract.author == self]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        total = 0
+        for contract in self.contracts():
+            total += contract.royalties
+        return total
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return [contract.author for contract in Contract.all if contract.book == self]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if isinstance(author, Author):
+            self.author = author
+        else:
+            raise Exception("author must be and instance of Author.")
+        if isinstance(book, Book):
+            self.book = book
+        else:
+            raise Exception("book must be an instance of Book.")
+        if type(date) == str:
+            self.date = date
+        else:
+            raise Exception("date must be a string.")
+        if type(royalties) == int:
+            self.royalties = royalties
+        else:
+            raise Exception("royaltiess but be an integer.")
+        Contract.all.append(self)
+
+    def contracts_by_date(date):
+        return [contract for contract in Contract.all if contract.date == date]
